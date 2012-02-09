f0b9e24c009db72bacaba025c4bc7486a56144ce
cgrymala <cgrymala@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-02-09 19:39:54 +0000

    Begin implementing cookies for tabs Implement closed accordion option
    git-svn-id: https://plugins.svn.wordpress.org/collapsible-widget-area/trunk@502883 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- class.collapsible-widget.php
+++ class.collapsible-widget.php
@@ -4,6 +4,9 @@
  */
 class collapsible_widget extends WP_Widget {
 	var $tabbed_accordion = null;
+	var $collapsible = false;
+	var $closed = false;
+	var $cookie = false;
 	/**
 	 * Construct our widget item
 	 */
@@ -29,7 +32,8 @@ class collapsible_widget extends WP_Widget {
 			$theme = $options['uitheme'];
 		}
 		$theme = apply_filters( 'collapsible-widget-ui-theme', $theme, $options['uitheme'] );
-		wp_register_script( 'collapsible-widgets', plugins_url( 'scripts/collapsible-widgets.js', __FILE__ ), array(), '0.2a', true );
+		wp_register_script( 'collapsible-widgets', plugins_url( 'scripts/collapsible-widgets.js', __FILE__ ), array(), '0.2.3a', true );
+		/*wp_register_script( 'jquery-cookie', plugins_url( 'scripts/jquery.cookie.js', __FILE__ ), array( 'jquery-ui' ), '1.0', true );*/
 		if ( ! empty( $theme ) )
 			wp_register_style( 'jquery-ui', $theme, array(), '1.8.17', 'screen' );
 		wp_register_style( 'collapsible-widgets', plugins_url( 'css/collapsible-widgets.css', __FILE__ ), array( 'jquery-ui' ), '0.2a', true );
@@ -38,7 +42,17 @@ class collapsible_widget extends WP_Widget {
 			wp_enqueue_style( 'collapsible-widgets' );
 	}
 	
+	function defaults() {
+		return apply_filters( 'collapsible-widget-defaults', array( 
+			'type'        => 'tabbed', 
+			'collapsible' => false, 
+			'closed'      => false, 
+			'cookie'      => false, 
+		) );
+	}
+	
 	function form( $instance ) {
+		$instance = wp_parse_args( $instance, $this->defaults() );
 		global $wp_registered_widgets, $collapsible_widget_area;
 		$widgets = wp_get_sidebars_widgets();
 		if ( ! array_key_exists( $collapsible_widget_area->sidebar_id, $widgets ) ) {
@@ -50,6 +64,9 @@ class collapsible_widget extends WP_Widget {
 	<p><?php _e( 'Display widgets in which manner?' ) ?><br/>
 		<input type="radio" name="<?php echo $this->get_field_name( 'show_what' ) ?>" id="<?php echo $this->get_field_id( 'show_what_tabbed' ) ?>" value="tabbed"<?php checked( $instance['show_what'], 'tabbed' ) ?>/> <label for="<?php echo $this->get_field_id( 'show_what_tabbed' ) ?>"><?php _e( 'Tabs' ) ?></label><br/>
 		<input type="radio" name="<?php echo $this->get_field_name( 'show_what' ) ?>" id="<?php echo $this->get_field_id( 'show_what_accordion' ) ?>" value="accordion"<?php checked( $instance['show_what'], 'accordion' ) ?>/> <label for="<?php echo $this->get_field_id( 'show_what_accordion' ) ?>"><?php _e( 'Accordion' ) ?></label></p>
+	<p><input type="checkbox" name="<?php echo $this->get_field_name( 'collapsible' ) ?>" id="<?php echo $this->get_field_id( 'collapsible' ) ?>" value="1"<?php checked( $instance['collapsible'] ) ?>/> <label for="<?php echo $this->get_field_id( 'collapsible' ) ?>"><?php _e( 'Allow entire accordion to be closed (if applicable)?' ) ?></label></p>
+	<p><input type="checkbox" name="<?php echo $this->get_field_name( 'closed' ) ?>" id="<?php echo $this->get_field_id( 'closed' ) ?>" value="1"<?php checked( $instance['closed'] ) ?>/> <label for="<?php echo $this->get_field_id( 'closed' ) ?>"><?php _e( 'Start with the entire accordion collapsed (only applicable if the above option is checked)' ) ?></label></p>
+	<p><input type="checkbox" name="<?php echo $this->get_field_name( 'cookie' ) ?>" id="<?php echo $this->get_field_id( 'cookie' ) ?>" value="1"<?php checked( $instance['cookie'] ) ?>/> <label for="<?php echo $this->get_field_id( 'cookie' ) ?>"><?php _e( 'Persist active tab across page views? (Currently only applicable to tabbed interface)' ) ?></label></p>
 <?php
 	}
 	
@@ -95,17 +112,24 @@ class collapsible_widget extends WP_Widget {
 			}
 		}
 		$instance['show_what'] = array_key_exists( 'show_what', $new_instance ) && 'accordion' == $new_instance['show_what'] ? 'accordion' : 'tabbed';
+		$instance['collapsible'] = in_array( $new_instance['collapsible'], array( '1', 1, true ) );
+		$instance['closed'] = in_array( $new_instance['closed'], array( '1', 1, true ) );
+		$instance['cookie'] = in_array( $new_instance['cookie'], array( '1', 1, true ) );
 		return $instance;
 	}
 	
 	function widget( $args, $instance ) {
 		extract( $args );
+		$instance = wp_parse_args( $instance, $this->defaults() );
 		global $collapsible_widget_area;
 		if ( ! is_active_sidebar( $collapsible_widget_area->sidebar_id ) )
 			return;
 		
 		add_action( 'wp_print_footer_scripts', array( $this, 'print_footer_scripts' ), 1 );
 		$this->tabbed_accordion = $instance['show_what'];
+		$this->collapsible = $instance['collapsible'];
+		$this->closed = $instance['closed'];
+		$this->cookie = $instance['cookie'];
 		echo $before_widget;
 		echo '<div class="collapsible-widget-container">';
 		dynamic_sidebar( $collapsible_widget_area->sidebar_id );
@@ -114,11 +138,16 @@ class collapsible_widget extends WP_Widget {
 	}
 	
 	function print_footer_scripts() {
+		$c = array();
+		$c['type'] = 'accordion' === $this->tabbed_accordion ? 'accordion' : 'tabbed';
+		$c['collapsible'] = $this->collapsible;
+		$c['closed'] = $this->closed;
+		$c['cookie'] = $this->cookie;
+		echo '<script type="text/javascript">var collapsible_widget_area = ' . json_encode( $c ) . ';</script>';
 		if ( 'accordion' === $this->tabbed_accordion ) {
-			echo '<script type="text/javascript">var collapsible_widget_area = { "type" : "accordion" };</script>';
 			wp_enqueue_script( 'jquery-ui-accordion' );
 		} else {
-			echo '<script type="text/javascript">var collapsible_widget_area = { "type" : "tabbed" };</script>';
+			wp_enqueue_script( 'jquery-cookie' );
 			wp_enqueue_script( 'jquery-ui-tabs' );
 		}
 		
